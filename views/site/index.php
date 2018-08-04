12022534f4d73560fdbd2f61020cdac8c931b593
Rodrigo Ferraz Mazurek <rodrigoferrazmazurek@hotmail.com>
2018-08-03 22:31:23 -0300

    entrega01

---
+++ views/site/index.php
@@ -0,0 +1,44 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Localiza</h1>
+
+        <p class="lead">Localiza locadora de veiculos. Segurança em primeiro lugar</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Reserva</h2>
+
+                <p>Ao fazer sua reserva de carros com a Referência Locadora você vai encontrar veículos sempre novos, além dos melhores preços e assistência 24 horas todos os dias da semana.Conheça o sistema de locação:</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Frota</h2>
+
+                <p>Na Referência você vai encontrar carros revisados regularmente. Conheça a frota:</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Comparativo</h2>
+
+                <p>Impostos, seguro e combustível são apenas alguns exemplos de gastos que não são planejados na hora da compra do veículo. O gasto final pode ser três vezes maior que o financiamento</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
