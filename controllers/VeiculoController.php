d21bfc8eeec92144acfa3bb6a9d9e7d778cdeb5a
Rodrigo Ferraz Mazurek <rodrigoferrazmazurek@hotmail.com>
2018-12-13 20:13:39 -0200

    projeto atualizado 12/12/2018
    Consegui arumar meu projeto o github removeu o  arquivo web.php ai criei novamente e voltou a funcionar.

---
+++ controllers/VeiculoController.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use app\models\Veiculo;
+use yii\data\ActiveDataProvider;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use yii\web\UploadedFile;
+
+/**
+ * VeiculoController implements the CRUD actions for Veiculo model.
+ */
+class VeiculoController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Veiculo models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $dataProvider = new ActiveDataProvider([
+            'query' => Veiculo::find() -> orderBy('id desc'),
+            'pagination' => [ 'pageSize' => 10, ], 
+            'sort' => [ 'attributes' => ['id']],
+        ]);
+
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single Veiculo model.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new Veiculo model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate() {
+        $model = new Veiculo();
+
+        if ($model->load(Yii::$app->request->post())) {
+            $model->save();
+            $fot = UploadedFile::getInstance($model, 'foto');
+            $imgName = 'fot_'. $model->id . '.' . $fot->getExtension();
+            $fot->saveAs(Yii::getAlias('@veiculoFotPath') .'\\' .$imgName);
+            $model->foto = $imgName;
+            $model->save();
+            return $this->redirect(['view', 'id' => $model->id]);
+        } 
+
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Updates an existing Veiculo model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Deletes an existing Veiculo model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Veiculo model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Veiculo the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Veiculo::findOne($id)) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+}
---
+++ models/Emprestimo.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "emprestimo".
+ *
+ * @property int $id
+ * @property int $placa
+ * @property string $data_emprestimo
+ * @property string $data_devolucao
+ * @property double $valor_locacao
+ * @property int $cliente
+ * @property int $funcionario
+ * @property int $ativo
+ *
+ * @property Locatario $cliente
+ * @property Funcionarios $funcionario
+ * @property Veiculo $placa
+ */
+class Emprestimo extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'emprestimo';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['placa', 'data_emprestimo', 'data_devolucao', 'valor_locacao', 'cliente', 'funcionario', 'ativo'], 'required'],
+            [['placa', 'cliente', 'funcionario', 'ativo'], 'integer'],
+            [['data_emprestimo', 'data_devolucao'], 'safe'],
+            [['valor_locacao'], 'number'],
+            [['cliente'], 'exist', 'skipOnError' => true, 'targetClass' => Locatario::className(), 'targetAttribute' => ['cliente' => 'id']],
+            [['funcionario'], 'exist', 'skipOnError' => true, 'targetClass' => Funcionarios::className(), 'targetAttribute' => ['funcionario' => 'id']],
+            [['placa'], 'exist', 'skipOnError' => true, 'targetClass' => Veiculo::className(), 'targetAttribute' => ['placa' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'placa' => 'Placa',
+            'data_emprestimo' => 'Data Emprestimo',
+            'data_devolucao' => 'Data Devolucao',
+            'valor_locacao' => 'Valor Locacao',
+            'cliente' => 'Cliente',
+            'funcionario' => 'Funcionario',
+            'ativo' => 'Ativo',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCliente()
+    {
+        return $this->hasOne(Locatario::className(), ['id' => 'cliente']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getFuncionario()
+    {
+        return $this->hasOne(Funcionarios::className(), ['id' => 'funcionario']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPlaca()
+    {
+        return $this->hasOne(Veiculo::className(), ['id' => 'placa']);
+    }
+}
